40017fd566d7585309d967e4ee8e3e6d25e8edb4
itsarnavsh <its.arnav.sh@gmail.com>
2023-12-18 16:45:21 +0530

    imp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
-int RESIDENTS,FLOORS,LIFTS;
 const int LIMIT = 6;
-class building
-{
-    public:
-    int RESIDENTS,FLOORS,LIFTS;
-    building();
-};
-building :: building()
-{
-    cout << "Enter the height of the building: ";
-    cin >> FLOORS;
-    cout << "Enter the number of residents in the building: ";
-    cin >> RESIDENTS;
-    cout << "Enter the number of lifts available: ";
-    cin >> LIFTS;
-}
 class elevator
 {
     protected:
@@ -40,14 +25,48 @@ class people
     elevator lift;
     int current_floor;
     int destination;
-    int direction;
+    int travelling;
+    //It will be 1 for up, -1 for down and 0 for not travelling
+    public:
+    people();
+    void spawn();
 };
+people :: people()
+{
+    current_floor = -1;
+    destination = -1;
+    travelling = 0;
+}
+void people :: spawn()
+{
+    current_floor = 2;//Something Random
+    destination = 5;//Something random
+    travelling = (destination - current_floor)>0?1:-1;
+}
 //////////////////////////////////////////////////
-
+class building
+{
+    public:
+    int RESIDENTS,FLOORS,LIFTS,RPF;
+    building();
+    people *members;
+    elevator *lifts;
+};
+building :: building()
+{
+    cout << "Enter the height of the building: ";
+    cin >> FLOORS;
+    cout << "Enter the number of residents in the building: ";
+    cin >> RESIDENTS;
+    cout << "Enter the number of lifts available: ";
+    cin >> LIFTS;
+    members = new people[RESIDENTS];
+    lifts = new elevator[LIFTS];
+}
 //////////////////////////////////////////////////
 int main()
 {
     building xyz;
-
+    
     return 0;
 }
